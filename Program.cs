ff81ee6e0ac403bbc0dda7b53d73ad114deb93e6
VITALII NAGIBIN <nagibin87@yandex.ru>
2022-05-18 18:55:49 +0300

    dz04
    dz04

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace vn_mode_csharp_dz04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int allPictures = 52;
+            int countPictureInLine = 3;
+            int countRows;
+            int leftPictures;
+
+            countRows = allPictures / countPictureInLine;
+            leftPictures = allPictures % countPictureInLine;
+
+            Console.WriteLine($"Полностью заполненных рядов будет {countRows}, а {leftPictures} картин(ка/нок) будет сверх меры.");
+        }
+    }
+}
